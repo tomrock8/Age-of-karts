40d07294981f6ba9bc836c1a1a4a1686130cf50a
Jordi Serna <sernavalero96@gmail.com>
2018-01-17 20:43:16 +0100

    Principio de implementacion GestorJugadores

---
+++ src/GestorJugadores.hpp
@@ -0,0 +1,34 @@
+
+#ifndef GESTORJUGADORES_H
+#define GESTORJUGADORES_H
+
+#include <iostream>
+#include <string>
+#include "IrrlichtLib.hpp"
+#include "Corredor.hpp"
+#include "CorredorRed.hpp"
+#include "CorredorJugador.hpp"
+
+using namespace std;
+
+class GestorJugadores
+{
+    public:
+
+        ~GestorJugadores(void);
+
+        static GestorJugadores *getInstancia();
+        Corredor **getJugadores();
+        
+
+        void setJugadores(Corredor **pj1);
+
+    private:
+        GestorJugadores(void);
+        static GestorJugadores *instancia;
+        Corredor **pj;
+
+
+};
+
+#endif

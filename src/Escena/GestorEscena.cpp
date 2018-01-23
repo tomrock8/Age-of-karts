44ed75bf85b048047981c24c33a3cf1630e70e34
Starfucker <tomrock8@gmail.com>
2018-01-23 20:47:51 +0100

    algo nuevo

--- src/Escena/GestorEscena.cpp
+++ src/Escena/GestorEscena.cpp
@@ -22,27 +22,25 @@ void GestorEscena::cambiaEscena(Escena::tipo_escena nueva) {
 	anterior->limpiar();
 
 	// Borrar la escena si es necesario
-	// NUEVA         ANTERIOR
-	// MENU		DESDE LOBBY		-> BORRAR LOBBY
-	// MENU		DESDE CARRERA	-> BORRAR CARRERA
-	// CARGA	DESDE LOBBY		-> BORRAR LOBBY
-	// CARRERA	DESDE CARGA		-> BORRAR CARGA
-
 	if (nueva == Escena::tipo_escena::MENU) {
+		// MENU		DESDE LOBBY		-> BORRAR LOBBY
 		if (anterior->getTipoEscena() == Escena::tipo_escena::LOBBY) {
 			// Borrar LOBBY
 		}
 		else {
+			// MENU		DESDE CARRERA	-> BORRAR CARRERA
 			if (anterior->getTipoEscena() == Escena::tipo_escena::CARRERA) {
 				// Borrar Carrera
 			}
 		}
 	}
 	else {
+		// CARGA	DESDE LOBBY		-> BORRAR LOBBY
 		if (nueva == Escena::tipo_escena::CARGA && anterior->getTipoEscena() == Escena::tipo_escena::LOBBY) {
 			// Borrar LOBBY
 		}
 		else {
+			// CARRERA	DESDE CARGA		-> BORRAR CARGA
 			if (nueva == Escena::tipo_escena::CARRERA && anterior->getTipoEscena() == Escena::tipo_escena::CARGA) {
 				// Borrar Carga
 			}
@@ -50,11 +48,35 @@ void GestorEscena::cambiaEscena(Escena::tipo_escena nueva) {
 	}
 
 	// Buscar si la nueva escena ya esta creada
-
 	// Crear si hace falta la nueva escena (porque no existe)
-
 	// Asignar la nueva escena a activa
+	if (!getEscena(nueva)) {
+		switch (nueva) {
+		case Escena::tipo_escena::CARGA:
+			//escenaActiva = new EscenaCarga(Escena::tipo_escena::CARGA);
+			break;
 
+		case Escena::tipo_escena::CARRERA:
+			escenaActiva = new EscenaJuego(Escena::tipo_escena::CARGA);
+			break;
+
+		case Escena::tipo_escena::CREDITOS:
+			//escenaActiva = new EscenaCreditos(Escena::tipo_escena::CREDITOS);
+			break;
+
+		case Escena::tipo_escena::LOBBY:
+			//escenaActiva = new EscenaLobby(Escena::tipo_escena::LOBBY);
+			break;
+
+		case Escena::tipo_escena::MENU:
+			//escenaActiva = new EscenaMenu(Escena::tipo_escena::MENU);
+			break;
+
+		case Escena::tipo_escena::OPCIONES:
+			//escenaActiva = new EscenaOpciones(Escena::tipo_escena::OPCIONES);
+			break;
+		}
+	}
 }
 
 GestorEscena *GestorEscena::getInstancia() {

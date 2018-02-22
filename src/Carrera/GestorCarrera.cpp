a8882375cf1194ed7f6016a930b0cf987f3dbfaf
Javier Martínez <jma96.elx@gmail.com>
2018-02-22 02:15:43 +0100

    Gestor de carrera optimizado

--- src/Carrera/GestorCarrera.cpp
+++ src/Carrera/GestorCarrera.cpp
@@ -38,47 +38,28 @@ bool GestorCarrera::update() {
 				pj_aux = pj2.at(k);
 				pj2.at(k) = pj2.at(k + 1);
 				pj2.at(k + 1) = pj_aux;
+			}else if (pj2.at(k)->getWaypointActual()->getWaypoint()->getID() == pj2.at(k+1)->getWaypointActual()->getWaypoint()->getID()) {  //si estan en el mismo waypoint
+				if (pj2.at(k)->getdistanciaWaypoint() > pj2.at(k+1)->getdistanciaWaypoint()) {     //si la distancia del waypoint pj1[y] es menor, su posicion en carrera aumenta (cont--)
+					pj_aux = pj2.at(k);
+					pj2.at(k) = pj2.at(k + 1);
+					pj2.at(k + 1) = pj_aux;
+				}
 			}
 		}
 	}
 	int aux = 0;
-	cont = 0;
-	int x = -1;
+	cont = 1;
 	int y = -1;
-	for (int num = vueltas; num >= 1; num--) {
-		pj_aux = NULL;
-		x = -1;
+	for (int num = vueltas+1; num >= 1; num--) {
 		y = -1;
 
 		//una vez ordenados por waypoints pasamos a clasificarlos, distinguiendo distancias entre waypoints en caso de ambiguedad (si estan el = waypoint)
 		for (int i = 0; i < jugadores->getNumJugadores(); i++) {
-			y = getCorredorIndexOriginal(i);          //cogemos el mismo corredor correspondiente en el array original pj1 (asociar pj1<-->pj2)
-			cont = jugadores->getNumJugadores();
-			if (num == pj1.at(y)->getVueltas()) {
-				aux++;
-				for (int j = 0; j < jugadores->getNumJugadores(); j++) {
-					if (i != j) {      //si no es el mismo entra
-						x = getCorredorIndexOriginal(j);  //hacemos lo mismo para el bucle actual (asociar pj1<-->pj2)
-						if (num == pj1.at(x)->getVueltas()) {
-							if (pj1.at(x)->getWaypointActual()->getWaypoint()->getID() == pj1.at(y)->getWaypointActual()->getWaypoint()->getID()) {  //si estan en el mismo waypoint
-								if (pj1.at(x)->getdistanciaWaypoint() > pj1.at(y)->getdistanciaWaypoint()) {     //si la distancia del waypoint pj1[y] es menor, su posicion en carrera aumenta (cont--)
-									cont--;
-								}
-							}
-							else {          //no estan en el mismo waypoint
-								if (i < j) {   //si el waypoint de i es mayor (i mas pequenya) entra
-									cont--;
-								}
-							}
-						}
-						else if (num > pj1.at(x)->getVueltas()) {
-							cont--;
-						}
-					}
-				}
-
-
+			y = getCorredorIndexOriginal(i);   
+			if (pj1.at(y)->getVueltas()==num){
+				      										 //cogemos el mismo corredor correspondiente en el array original pj1 (asociar pj1<-->pj2)
 				pj1.at(y)->setPosicionCarrera(cont);       //modificamos posicion en pj1[y]
+				cont++;
 			}
 		}
 	}

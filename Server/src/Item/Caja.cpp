6f69905e9d8e07f0fb11915d7090d03af38178d2
Unknown <sernavalero96@gmail.com>
2018-03-06 18:09:29 +0100

    Objetos online :D

--- Server/src/Item/Caja.cpp
+++ Server/src/Item/Caja.cpp
@@ -88,7 +88,7 @@ void Caja::romper(Corredor *pj1Col) {
 			ISceneNode *Node = static_cast<ISceneNode *>(Object->getUserPointer());
 			Node->setVisible(false);
 			timer = time->getTimer();
-			if (pj1Col->getTipoObj() == 0 && strcmp(pj1Col->getNodo()->getName(), "JugadorRed") != 0) {
+			if (pj1Col->getTipoObj() == 0) {
 				std::cout<< "Entro al objeto\n";
 				pj1Col->setTipoObj();
 			}

ea3efb67cd70650c2cab1c13b9b84bccde442d42
Unknown <salvamateo15@gmail.com>
2018-04-05 17:40:40 +0200

    Menu Principal integrado

--- src/Escena/EscenaMenu.cpp
+++ src/Escena/EscenaMenu.cpp
@@ -30,12 +30,24 @@ EscenaMenu::EscenaMenu() : Escena(Escena::tipo_escena::MENU) {
 	logoAOK = Motor3d::instancia().getDriver()->getTexture("assets/logoAOK.png");
 	Motor3d::instancia().getDriver()->makeColorKeyTexture(logoAOK, core::position2d<s32>(0, 0));*/
 
+	//Se asigna la opcion del menu al primer boton
+	optionMenu = 0;
+
+	//Se crea el hud del menu principal
 	TMotor::instancia().newHud("MainMenuHUD");
-	TMotor::instancia().getActiveHud()->addElement(1.2f, 0.8f, "logo", "assets/HUD/MainMenu/logo.png");
-	TMotor::instancia().getHud("MainMenuHUD")->traslateElement("logo", 0.0f, 0.55f);
-	TMotor::instancia().getActiveHud()->addElement(0.4f, 0.15f, "local", "assets/HUD/MainMenu/local.png");
-	TMotor::instancia().getActiveHud()->addElement(0.4f, 0.15f, "online", "assets/HUD/MainMenu/online.png");
-	TMotor::instancia().getHud("MainMenuHUD")->traslateElement("online", -0.01f, -0.2f);
+	//Se añaden los distintos elementos del hud y se posicionan correctamente
+	TMotor::instancia().getActiveHud()->addElement(1.0f, 1.0f, "logo", "assets/HUD/MainMenu/aok_logo.png");
+	//--LOGO--
+	TMotor::instancia().getHud("MainMenuHUD")->traslateElement("logo", 0.0f, 0.45f);
+	TMotor::instancia().getActiveHud()->addElement(0.4f, 0.15f, "local", "assets/HUD/MainMenu/btn_local_normal.png");
+	//--BOTONES--
+	TMotor::instancia().getHud("MainMenuHUD")->traslateElement("local", -0.01f, -0.1f);
+	TMotor::instancia().getActiveHud()->addElement(0.4f, 0.15f, "online", "assets/HUD/MainMenu/btn_online_normal.png");
+	TMotor::instancia().getHud("MainMenuHUD")->traslateElement("online", -0.01f, -0.3f);
+	TMotor::instancia().getActiveHud()->addElement(0.4f, 0.15f, "opciones", "assets/HUD/MainMenu/btn_opciones_normal.png");
+	TMotor::instancia().getHud("MainMenuHUD")->traslateElement("opciones", -0.01f, -0.5f);
+	TMotor::instancia().getActiveHud()->addElement(0.4f, 0.15f, "salir", "assets/HUD/MainMenu/btn_salir_normal.png");
+	TMotor::instancia().getHud("MainMenuHUD")->traslateElement("salir", -0.01f, -0.7f);
 
 }
 
@@ -46,8 +58,6 @@ EscenaMenu::~EscenaMenu() {
 }
 
 void EscenaMenu::init() {
-
-
 }
 
 void EscenaMenu::dibujar() {
@@ -67,15 +77,83 @@ void EscenaMenu::limpiar() {
 }
 
 void EscenaMenu::update() {
+
+	//Modificamos los botones para responder a las acciones del usuario
+	TMotor::instancia().getActiveHud()->changeTextureElement("local", "assets/HUD/MainMenu/btn_local_normal.png");
+	TMotor::instancia().getActiveHud()->changeTextureElement("online", "assets/HUD/MainMenu/btn_online_normal.png");
+	TMotor::instancia().getActiveHud()->changeTextureElement("opciones", "assets/HUD/MainMenu/btn_opciones_normal.png");
+	TMotor::instancia().getActiveHud()->changeTextureElement("salir", "assets/HUD/MainMenu/btn_salir_normal.png");
+
+	TMotor::instancia().getActiveHud()->scaleElement("local",1.0f,1.0f);
+	TMotor::instancia().getActiveHud()->scaleElement("online",1.0f,1.0f);
+	TMotor::instancia().getActiveHud()->scaleElement("opciones",1.0f,1.0f);
+	TMotor::instancia().getActiveHud()->scaleElement("salir",1.0f,1.0f);
+
+	if (optionMenu == 0){
+		TMotor::instancia().getActiveHud()->changeTextureElement("local", "assets/HUD/MainMenu/btn_local_pulsado.png");
+		TMotor::instancia().getActiveHud()->scaleElement("local",1.2f,1.2f);
+	}else if (optionMenu == 1){
+		TMotor::instancia().getActiveHud()->changeTextureElement("online", "assets/HUD/MainMenu/btn_online_pulsado.png");
+		TMotor::instancia().getActiveHud()->scaleElement("online",1.2f,1.2f);
+	}else if (optionMenu == 2){
+		TMotor::instancia().getActiveHud()->changeTextureElement("opciones", "assets/HUD/MainMenu/btn_opciones_pulsado.png");
+		TMotor::instancia().getActiveHud()->scaleElement("opciones",1.2f,1.2f);
+	}else{
+		TMotor::instancia().getActiveHud()->changeTextureElement("salir", "assets/HUD/MainMenu/btn_salir_pulsado.png");
+		TMotor::instancia().getActiveHud()->scaleElement("salir",1.2f,1.2f);
+	}
 }
 
 Escena::tipo_escena EscenaMenu::comprobarInputs() {
 
+	//Si se pulsa ESCAPE se sale directamente del juego
 	if (glfwGetKey(TMotor::instancia().getVentana(), GLFW_KEY_ESCAPE) == GLFW_PRESS){
 		if(!pressed){
 			return Escena::tipo_escena::SALIR; // Devuelve el estado de las escenas para que salga
 		}
-	}else if (glfwGetKey(TMotor::instancia().getVentana(), GLFW_KEY_1) == GLFW_PRESS){
+	}
+	
+	//Segun se toque arriba o abajo, el usuario se va moviendo entre los distintos botones del menu
+	if (glfwGetKey(TMotor::instancia().getVentana(), GLFW_KEY_DOWN) == GLFW_PRESS){
+		if (!pressed){
+			if (optionMenu == 3){
+				optionMenu = 0;
+			}else{
+				optionMenu++;
+			}
+			pressed = true;
+		}
+	}else if (glfwGetKey(TMotor::instancia().getVentana(), GLFW_KEY_UP) == GLFW_PRESS){
+		if (!pressed){
+			if (optionMenu == 0){
+				optionMenu = 3;
+			}else{
+				optionMenu--;
+			}
+			pressed = true;
+		}
+	}else pressed = false;
+
+	//Si se pulsa intro se comprueba en que opcion del menu se encuentra el usuario y se cambia a la escena correspondiente
+	if (glfwGetKey(TMotor::instancia().getVentana(), GLFW_KEY_ENTER) == GLFW_PRESS){
+		if (optionMenu == 0){
+			cout << "SINGLEPLAYER\n";
+			ipConexion="offline";
+			return Escena::tipo_escena::LOBBY;
+		}else if (optionMenu == 1){
+			cout << "MULTIPLAYER\n";
+			ipConexion="";
+			return Escena::tipo_escena::LOBBY;
+		}else if (optionMenu == 2){
+			cout << "OPCIONES\n";
+		}else{
+			cout << "SALIR\n";
+			return Escena::tipo_escena::SALIR; 
+		}
+	}
+
+
+	/*else if (glfwGetKey(TMotor::instancia().getVentana(), GLFW_KEY_1) == GLFW_PRESS){
 		cout << "SINGLE PLAYER\n";
 		ipConexion="offline";
 		return Escena::tipo_escena::LOBBY;
@@ -90,7 +168,9 @@ Escena::tipo_escena EscenaMenu::comprobarInputs() {
 		cout << "CREDITOS\n";
 		return Escena::tipo_escena::CREDITOS;
 	}else pressed = false;
+	*/
 
+	//Se retorna la escena Menu (donde estamos) en caso de que no se haya cambiado de pantalla
 	return Escena::tipo_escena::MENU;
 }
 std::string EscenaMenu::getIpConexion(){

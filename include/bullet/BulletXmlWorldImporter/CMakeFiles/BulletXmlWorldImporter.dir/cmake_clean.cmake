fc5c0d3f7a8e691472441c8daa47027d8a0b0433
Starfucker <tomrock8@gmail.com>
2018-04-06 20:26:32 +0200

    wadafak?

---
+++ include/bullet/BulletXmlWorldImporter/CMakeFiles/BulletXmlWorldImporter.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BulletXmlWorldImporter.dir/btBulletXmlWorldImporter.o"
+  "CMakeFiles/BulletXmlWorldImporter.dir/string_split.o"
+  "CMakeFiles/BulletXmlWorldImporter.dir/tinyxml.o"
+  "CMakeFiles/BulletXmlWorldImporter.dir/tinystr.o"
+  "CMakeFiles/BulletXmlWorldImporter.dir/tinyxmlerror.o"
+  "CMakeFiles/BulletXmlWorldImporter.dir/tinyxmlparser.o"
+  "libBulletXmlWorldImporter.pdb"
+  "libBulletXmlWorldImporter.so"
+  "libBulletXmlWorldImporter.so.2.88"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BulletXmlWorldImporter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

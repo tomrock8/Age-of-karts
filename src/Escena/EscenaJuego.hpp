3d988b23218da0f256dedc0a0bc698a777414851
Starfucker <tomrock8@gmail.com>
2018-04-24 11:32:42 +0200

    gravedad en tiempo real

--- src/Escena/EscenaJuego.hpp
+++ src/Escena/EscenaJuego.hpp
@@ -43,6 +43,8 @@ private:
 
 	void UpdatePhysics(unsigned int TDeltaTime);
 	void UpdateRender(btRigidBody *TObject);
+	void renderDebug();
+
 	bool debug;
 
 	DebugDraw *debugDraw;
@@ -65,12 +67,14 @@ private:
 	bool fin_carrera;
 	Timer *t;
 
+	float gravedad;
+
 	// IMGUI
 	bool muestraDebug;
 	bool debug_Jugador;
 	bool end;
 	
-	void renderDebug();
+	
 };
 
 #endif /* ESCENAJUEGO_H */

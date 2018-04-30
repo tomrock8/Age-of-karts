615b60382644841917028c54db4eeed880c3edbb
Starfucker <tomrock8@gmail.com>
2018-04-30 12:21:26 +0200

    Clase TAnimacion creada

--- src/OpenGL/ArbolEscena/TAnimacion.cpp
+++ src/OpenGL/ArbolEscena/TAnimacion.cpp
@@ -1,28 +1,22 @@
-#include "TMalla.hpp"
-
-TMalla::TMalla(std::vector <mesh *> m) {
+#include "TAnimacion.hpp"
+/*
+TAnimacion::TAnimacion(std::vector <mesh *> m) {
 	//los vertices e indices son punteros a Trecurso malla 7
 	malla = m;
 	visible = true;
 }
+*/
 
-bool TMalla::isVisible(){
-	return visible;
-}
-
-void TMalla::setVisible(bool visible){
-	this->visible = visible;
-}
-
-void TMalla::beginDraw(Shader *shader) {// Depende del tipo de entidad
-	/* Llamar al metodo de dibujado del recurso que se encarga del dibujado 
-	en OpenGL de los poligonos a partir de los vertices, las normales y 
-	las coordenadas de textura(Gestor) */
+bool TAnimacion::isVisible() { return visible; }
+void TAnimacion::setVisible(bool visible) { this->visible = visible; }
+/*
+void TAnimacion::beginDraw(Shader *shader) {
 	if(visible)
 		draw(shader);
 }
-
-void TMalla::draw(Shader *shader) {
+*/
+/*
+void TAnimacion::draw(Shader *shader) {
 	for (GLuint i = 0; i < malla.size(); i++) {
 		//Pasamos la modelMatrix de la malla al shader para calcular la posicion final
 		shader->setMat4("model", modelMatrix);
@@ -32,7 +26,7 @@ void TMalla::draw(Shader *shader) {
 			//Activamos la textura de la malla
 			malla.at(i)->getText()->activeTexture(shader);
 		}
-			
+
 		//Activamos el material
 		malla.at(i)->getMat()->activeMaterial(shader);
 		// Se llama al dibujado de la malla
@@ -42,10 +36,15 @@ void TMalla::draw(Shader *shader) {
 			//Desactivamos las texturas usadas
 			malla.at(i)->getText()->disableTexture();
 		}
-		
+
 		//Desacivamos el buffer VAO antes del dibujado de la siguiente malla
 		malla.at(i)->getMesh()->disableVAO();
 	}
-}
 
-void TMalla::endDraw() {}
+
+
+}
+*/
+/*
+void TAnimacion::endDraw() {}
+*/
